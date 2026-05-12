3694e86021dd8bb236fadff9f6092d744662bbaa
Jannatul Ferdous <jannatulferdous8558@gmail.com>
2026-05-12 14:58:55 +0600

    first commit

---
+++ src/main/java/com/mycellium/mycellium/model/Event.java
@@ -0,0 +1,38 @@
+//package com.mycellium.mycellium.model;
+//
+//public class Event {
+//
+//    private String title;
+//    private String date;
+//    private String location;
+//    private String category;
+//    private String image;
+//
+//    public Event(String title, String date, String location, String category, String image) {
+//        this.title = title;
+//        this.date = date;
+//        this.location = location;
+//        this.category = category;
+//        this.image = image;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public String getDate() {
+//        return date;
+//    }
+//
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    public String getCategory() {
+//        return category;
+//    }
+//
+//    public String getImage() {
+//        return image;
+//    }
+//}
---
+++ src/test/java/com/mycellium/mycellium/MycelliumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycellium.mycellium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycelliumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
